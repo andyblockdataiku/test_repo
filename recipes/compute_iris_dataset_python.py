ae44446e63a449b61efb7736bd7e8de565d89164
admin <admin>
2022-02-23 09:04:22 -0500

    Recipe RBC_CODE_DEMO.compute_iris_dataset_python saved through notebook notebook editor for compute_iris_dataset_python

--- recipes/compute_iris_dataset_python.py
+++ recipes/compute_iris_dataset_python.py
@@ -1,17 +1,17 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+from sklearn.datasets import load_iris
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+iris = load_iris()
 
+iris_dataset_python_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']], 
+                                      columns= iris['feature_names'] + ['target'])
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-iris_dataset_python_df = ... # Compute a Pandas dataframe to write into iris_dataset_python
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 iris_dataset_python = dataiku.Dataset("iris_dataset_python")
 iris_dataset_python.write_with_schema(iris_dataset_python_df)
